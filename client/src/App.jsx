26a40d3be411d02e9d2b91f80f8958e5932db395
User <user@Users-MacBook-Air.local>
2024-03-18 19:35:24 -0400

    added Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Project 3</h1>
+      <Navbar />
       <Cart />
       <Home />
       <Signup />
