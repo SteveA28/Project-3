a3038874d80a55587bafff8982364b036cf5a1a4
SteveA28 <alonsosteven32@gmail.com>
2024-03-23 17:29:58 -0400

    added logged in and logout functions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CartProvider } from "./contexts/CartContext"; // Import the CartProvider
+import { AuthProvider } from "./contexts/AuthContext"; // Import the AuthProvider
 import Cart from "./components/Cart";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
@@ -10,16 +11,13 @@ import PackageDetails from "./components/PackageDetails";
 
 function App() {
   return (
-    <>
-      <CartProvider>
-        {" "}
-        {/* Wrap your app with CartProvider */}
+    <AuthProvider> {/* Wrap everything within AuthProvider */}
+      <CartProvider> {/* Wrap your app with CartProvider */}
         <BrowserRouter>
-          <Navbar />
+          <Navbar /> {/* Navbar will now have access to AuthContext */}
           <Routes>
             <Route path="/" element={<Home />}></Route>
-            <Route path="/cart" element={<Cart />}></Route>{" "}
-            {/* Ensure you have a Cart route */}
+            <Route path="/cart" element={<Cart />}></Route> {/* Ensure you have a Cart route */}
             <Route path="signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             {/* <Route path="/details" element={<PackageDetails />} /> */}
@@ -27,7 +25,7 @@ function App() {
           </Routes>
         </BrowserRouter>
       </CartProvider>
-    </>
+    </AuthProvider>
   );
 }
 
