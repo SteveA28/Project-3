9a17d61191776e2fa52cd4282ffe9f80ad87a662
User <user@Users-MacBook-Air.local>
2024-03-23 14:34:46 -0400

    added OR operator to mongoose.connect in case process.env does not work

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,7 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 try {
-  await mongoose.connect(process.env.MONGO_URI, { dbName: "Project3" });
+  await mongoose.connect(
+    process.env.MONGO_URI ||
+      "mongodb+srv://Rilwan410:Nawlir41001@cluster0.frzazvm.mongodb.net/",
+    { dbName: "Project3" }
+  );
   console.log("Successfully connected to Project 3 Database");
 } catch (e) {
   console.log(e);
