ee3c61e35098453fb671dc636f9596dec96ab01d
SteveA28 <alonsosteven32@gmail.com>
2024-03-25 20:26:11 -0400

    added datepicker

--- client/src/contexts/CartContext.jsx
+++ client/src/contexts/CartContext.jsx
@@ -9,15 +9,23 @@ export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
 
   const addToCart = (item) => {
-    setCartItems(currentItems => [...currentItems, item]);
+    setCartItems(currentItems => [...currentItems, { ...item, startDate: null, endDate: null }]);
   };
 
   const removeFromCart = (index) => {
     setCartItems(currentItems => currentItems.filter((_, i) => i !== index));
   };
 
+  const updateDateRange = (index, startDate, endDate) => {
+    setCartItems(currentItems =>
+      currentItems.map((item, i) =>
+        i === index ? { ...item, startDate, endDate } : item
+      )
+    );
+  };
+
   return (
-    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, updateDateRange }}>
       {children}
     </CartContext.Provider>
   );
